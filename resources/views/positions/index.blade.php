18d935a8919a8b433f4322dfdb92c82ef13375d0
Tomáš Kozelek <tommyside@centrum.sk>
2026-09-15 08:53:28 +0200

    widen positions page so table doesn't need horizontal scroll

--- resources/views/positions/index.blade.php
+++ resources/views/positions/index.blade.php
@@ -1,5 +1,5 @@
 <x-layout title="Pozície" description="Pracovné pozície, z ktorých sa skladá rozpis zmien.">
-    <div class="container mx-auto max-w-5xl px-4 py-8 sm:px-6 lg:px-8">
+    <div class="container mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
         <div class="flex flex-col gap-6">
 
             <x-page-header
